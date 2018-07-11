1bea7e7d8e1792ea4e809ad9615f8c0abbabd760
John Beckius <jbeckius@jd-10151.local>
2018-07-11 14:24:32 -0400

    Added getAccessToken action to store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import apiManager from './api/apiManager.js';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+		apps: [],
+		accessToken: null
   },
   mutations: {
-
+		updateApps(state, apps) {
+			state.apps = apps;
+		},
+		setAccessToken(state, token) {
+			state.accessToken = token;
+		}
   },
   actions: {
+		updateApps({ commit }) {
 
+		},
+		getAccessToken({ commit }, creds) {
+			return apiManager.Token.getToken(creds)
+			.then(resp => {
+				let token = resp.data.access_token;
+				localStorage.set('access_token', token);
+				return commit( 'setAccessToken', token );
+			})
+
+		}
   }
 })
