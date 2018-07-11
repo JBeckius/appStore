67982296394834f8e932f2fb3fdda9b464bc8422
John Beckius <jbeckius@jd-10151.local>
2018-07-11 11:39:39 -0400

    Add modalMixin file

---
+++ src/components/modals/appModalMixin.js
@@ -0,0 +1,17 @@
+import moment from 'moment'
+
+export default {
+	computed: {
+		latestVersion() {
+			let versions = this.appData.versions;
+			return (versions && versions.length) > 0 ? versions[0].version : "No version #";
+		},
+		fileSize() {
+			let versions = this.appData.versions;
+			return (versions && versions.length) > 0 ? versions[0].fileSize : "No file size";
+		},
+		lastUpdatedDate() {
+			return moment(this.appData.lastUpdated).format("MM/DD/YYYY");
+		}
+	}
+}
