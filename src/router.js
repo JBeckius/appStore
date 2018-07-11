5fbe04ab32f77e24c6fc7b095b389e9646e636ce
John Beckius <jbeckius@jd-10151.local>
2018-07-11 14:22:37 -0400

    Added login and auth to router

--- src/router.js
+++ src/router.js
@@ -6,10 +6,11 @@ import Directory from './views/Directory.vue'
 import UploadApp from './views/UploadApp.vue'
 import DownloadReport from './views/DownloadReport.vue'
 import ConfigMapping from './views/ConfigMapping.vue'
+import Login from './views/Login.vue'
 
 Vue.use(Router)
 
-export default new Router({
+let router =  new Router({
 	base: '/app',
   routes: [
     {
@@ -17,6 +18,14 @@ export default new Router({
       name: 'home',
       component: Home
     },
+		{
+			path: '/login',
+			name: 'login',
+			component: Login,
+			meta: {
+				guest: true
+			}
+		},
     {
       path: '/about',
       name: 'about',
@@ -25,22 +34,68 @@ export default new Router({
 		{
 			path: '/directory',
 			name: 'directory',
-			component: Directory
+			component: Directory,
+			meta: {
+				requiresAuth: true
+			}
 		},
 		{
 			path: '/uploadApp',
 			name: 'uploadApp',
-			component: UploadApp
+			component: UploadApp,
+			meta: {
+				requiresAuth: true
+			}
 		},
 		{
 			path: '/downloadReport',
 			name: 'downloadReport',
-			component: DownloadReport
+			component: DownloadReport,
+			meta: {
+				requiresAuth: true
+			}
 		},
 		{
 			path: '/configMapping',
 			name: 'configMapping',
-			component: ConfigMapping
+			component: ConfigMapping,
+			meta: {
+				requiresAuth: true
+			}
 		}
   ]
 })
+
+router.beforeEach((to, from, next) => {
+    if(to.matched.some(record => record.meta.requiresAuth)) {
+        if (localStorage.getItem('access_token') == null) {
+            next({
+                path: '/login',
+                params: { nextUrl: to.fullPath }
+            })
+        } else {
+            let user = JSON.parse(localStorage.getItem('user'))
+            if(to.matched.some(record => record.meta.is_admin)) {
+                if(user.is_admin == 1){
+                    next()
+                }
+                else{
+                    next({ name: 'userboard'})
+                }
+            }else {
+                next()
+            }
+        }
+    } else if(to.matched.some(record => record.meta.guest)) {
+        if(localStorage.getItem('access_token') == null){
+            next()
+        }
+        else{
+            next({ name: 'userboard'})
+        }
+    }else {
+        next()
+    }
+})
+
+export default router;
