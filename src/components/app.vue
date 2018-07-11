d8029438b79a4c8c989ff2dcf737eb0ed33419a5
John Beckius <jbeckius@jd-10151.local>
2018-07-11 10:53:27 -0400

    Added app selection method to all appNodes

--- src/components/app.vue
+++ src/components/app.vue
@@ -10,15 +10,15 @@
 					<h5 class="card-title">{{appData.applicationName}}</h5>
 					<!-- <p class="card-text">Version {{latestVersion version}}</p> -->
 					<p class="card-text">Version 1.x</p>
-					<button class="btn btn-primary settingsBtn" :data-id="appData.applicationId">
+					<button class="btn btn-primary settingsBtn" :data-id="appData.applicationId" v-on:click="select()">
 						Settings
 					</button>
-					<!-- <a v-if="iOS" :href="href" class="btn btn-secondary">Download</a>
-					<a v-if="!iOS" :href="href" class="btn btn-secondary downloadBtn" :data-url="appData.applicationId">Download</a> -->
+					<a v-if="iOS" :href="href" class="btn btn-secondary">Download</a>
+					<a v-if="!iOS" :href="href" class="btn btn-secondary downloadBtn" :data-url="appData.applicationId">Download</a>
 				</div>
 			</div>
 		</div>
-		<SettingsModal :appData="appData" />
+		<!-- <SettingsModal :appData="appData" /> -->
 	</div>
 </template>
 
@@ -28,15 +28,25 @@ import SettingsModal from './modals/settingsModal.vue';
 export default {
 	name: 'appNode',
 	components: {SettingsModal},
-	props: ['appData'],
+	props: ['appData', 'selectApp', 'index'],
 	data() {
 		return {
-			iOS: (!!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform))
+			iOS: (!!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform)),
+
+		}
+	},
+	methods: {
+		select() {
+
+				this.selectApp(this.index);
+
 		}
 	},
 	computed: {
-		// href: this.iOS ? `itms-services://?action=download-manifest&url=${this.appData.versions[1].path}`
-		// 							 : "#",
+		href(){
+			return this.iOS ? `itms-services://?action=download-manifest&url=${this.appData.versions[1].path}`
+										  : "#";
+		} ,
 		// iOS: (!!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform))
 	}
 }
