e0d6a32dbe88147d03189cbb64445b13fd452b80
John Beckius <jbeckius@jd-10151.local>
2018-07-12 13:57:25 -0400

    Moved store reference

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/store'
 import apiManager from './api/apiManager.js'
 import 'bootstrap'
 import "bootstrap/scss/bootstrap.scss"
