a7e038f88ce48d5ed73876ff1fc310a7de72c2b6
John Beckius <jbeckius@jd-10151.local>
2018-07-18 13:35:47 -0400

    Broke fileuploader input into its own component

---
+++ src/components/fileUpload.vue
@@ -0,0 +1,120 @@
+<template>
+	<form enctype="multipart/form-data" novalidate v-if="isInitial || isSaving">
+		<div class="dropbox">
+			<input type="file"
+						:name="name"
+						:disabled="isSaving"
+						@change="upload($event.target.name, $event.target.files[0])"
+						:accept="fileType"
+						class="input-file"
+			/>
+	    <p v-if="isInitial">Drag your file(s) here to begin<br /> or click to browse</p>
+	    <p v-if="isSaving">Uploading file</p>
+	  </div>
+	</form>
+</template>
+
+<script>
+	import apiManager from '../api/apiManager.js';
+
+	const STATUS_INITIAL = 0, STATUS_SAVING = 1, STATUS_SUCCESS = 2, STATUS_FAILED = 3;
+
+	export default {
+		name: 'fileUploader',
+		props: ['fileType', 'name', 'upload'],
+		data() {
+			return {
+				uploadedFile: null,
+				uploadError: null,
+				currentStatus: null,
+				uploadFieldName: 'photos'
+			}
+		},
+		computed: {
+			isInitial() {
+				return this.currentStatus === STATUS_INITIAL;
+			},
+			isSaving() {
+				return this.currentStatus === STATUS_SAVING;
+			},
+			isSuccess() {
+				return this.currentStatus === STATUS_SUCCESS;
+			},
+			isFailed() {
+				return this.currentStatus === STATUS_FAILED;
+			}
+		},
+		methods: {
+			reset() {
+				// reset form to initial state
+				this.currentStatus = STATUS_INITIAL;
+				this.uploadedFile = null;
+				this.uploadError = null;
+			},
+			save(formData) {
+				// upload data to the server
+				this.currentStatus = STATUS_SAVING;
+
+				upload(formData)
+					.then(x => {
+						this.uploadedFile = x;
+						this.currentStatus = STATUS_SUCCESS;
+					})
+					.catch(err => {
+						this.uploadError = err.response;
+						this.currentStatus = STATUS_FAILED;
+					});
+			},
+			filesChange(fieldName, fileList) {
+				// handle file changes
+				const formData = new FormData();
+
+				if (!fileList.length) return;
+
+				// append the files to FormData
+				Array
+					.from(Array(fileList.length).keys())
+					.map(x => {
+						formData.append(fieldName, fileList[x], fileList[x].name);
+					});
+
+				// save it
+				this.save(formData);
+			}
+		},
+		mounted() {
+			this.reset();
+		},
+	}
+</script>
+
+<style scoped>
+  .dropbox {
+    outline: 2px dashed grey; /* the dash box */
+    outline-offset: -10px;
+    background: lightcyan;
+    color: dimgray;
+    padding: 10px 10px;
+    min-height: 200px; /* minimum height */
+    position: relative;
+    cursor: pointer;
+  }
+
+  .input-file {
+    opacity: 0; /* invisible but it's there! */
+    width: 100%;
+    height: 200px;
+    position: absolute;
+    cursor: pointer;
+  }
+
+  .dropbox:hover {
+    background: lightblue; /* when mouse over to the drop zone, change color */
+  }
+
+  .dropbox p {
+    font-size: 1.2em;
+    text-align: center;
+    padding: 50px 0;
+  }
+</style>
