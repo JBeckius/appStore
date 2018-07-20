6c8edbe22ab3ac7d57f81aaf7d92adf73ef254dc
John Beckius <jbeckius@jd-10151.local>
2018-07-20 10:46:08 -0400

    added upload request to apps api obj

--- src/api/apps.js
+++ src/api/apps.js
@@ -6,5 +6,11 @@ export class Apps extends baseCRUDAPI {
 		super(baseURLPath);
 		this.apiBaseName = 'app';
 	}
-
+	upload(app) {
+		return this.axiosObject.post(this.apiBaseName, app, {
+			headers: {
+				'Content-type': 'application/json'
+			}
+		})
+	}
 }
