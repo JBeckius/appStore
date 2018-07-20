be75a67bb66c9bdc968440ac993e62f20287568d
John Beckius <jbeckius@jd-10151.local>
2018-07-20 16:31:43 -0400

    add addAD request to groups api obj

--- src/api/groups.js
+++ src/api/groups.js
@@ -7,4 +7,17 @@ export class Groups extends baseCRUDAPI {
 		this.apiBaseName = 'group';
 	}
 
+	addAD({clientName, adName, id}={}) {
+		let data = {
+			clientName,
+			adName,
+			id
+		};
+		return this.axiosObject.post(this.apiBaseName + '/AD', data, {
+			headers: {
+				"Content-Type":"application/json"
+			}
+		});
+	}
+
 }
