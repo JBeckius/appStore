2935d2c29f895f7de1254a49278165ff2bee3b74
John Beckius <jbeckius@jd-10151.local>
2018-07-20 10:44:15 -0400

    Created some custom input components

---
+++ src/components/inputs/checkbox.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="col-auto my-1">
+		<div class="form-check">
+			<input v-bind:value="value"
+						 v-on:input="$emit('input', $event.target.checked)"
+						 class="form-check-input"
+						 type="checkbox"
+						 :name="name"
+						 :checked="checked"
+			>
+			<label class="form-check-label" :for="name">
+				{{ label }}
+			</label>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'checkbox',
+		props: ['value', 'name', 'label', 'checked']
+	}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/inputs/dropdown.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="form-group col-md-4 my-1">
+		<select v-bind:value="value"
+		 				v-on:input="$emit('input', $event.target.value)"
+						class="form-control"
+						id="clients"
+						:name="name"
+						required
+		>
+			<option :value="{}" class="dropDownPlaceHolder">{{placeholder}}</option>
+			<option v-if="options" v-for="(option, index) in options" :value="option[optionValue]" :key="index">{{option[optionDisplay]}}</option>
+		</select>
+		<div class="d-none" id="curcli"></div>
+		<div class="invalid-feedback">
+			{{ placeholder }}
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'dropdown',
+		props: ['value', 'options', 'optionValue', 'optionDisplay', 'placeholder', 'name']
+	}
+</script>
+
+<style scoped>
+</style>
