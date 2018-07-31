e186dcb805233bca0bdeb8547f06f69a4b2e7695
John Beckius <jbeckius@jd-10151.local>
2018-07-31 10:27:53 -0400

    Added download method to executables api object

--- src/api/executables.js
+++ src/api/executables.js
@@ -26,4 +26,41 @@ export class Executables extends baseCRUDAPI {
 		});
 	}
 
+	download(exeId) {
+		return this.axiosObject.get(this.apiBaseName + '/download/apk/'+ exeId, {exeId}, {
+			headers: {
+				'contentType': 'application/json'
+			}
+		})
+			.then(resp => {
+				console.log(resp);
+				const blob = this.b64toBlob(resp.data, resp.headers['content-type']);
+				const blobUrl = URL.createObjectURL(blob);
+				let a = document.createElement('a');
+				a.href = blobUrl;
+				a.download = resp.headers['x-filename'];
+				a.click();
+				window.URL.revokeObjectURL(blobUrl);
+
+			})
+	}
+
+	b64toBlob(b64Data, contentType = '', sliceSize = 512) {
+	const byteCharacters = atob(b64Data);
+	const byteArrays = [];
+	for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+		const slice = byteCharacters.slice(offset, offset + sliceSize);
+		const byteNumbers = new Array(slice.length);
+		for (let i = 0; i < slice.length; i++) {
+			byteNumbers[i] = slice.charCodeAt(i);
+		}
+		const byteArray = new Uint8Array(byteNumbers);
+		byteArrays.push(byteArray);
+	}
+	return new Blob(byteArrays, {
+		type: contentType
+	});
+
+}
+
 }
