70f2823f57de6a2e0adfd493adbfe84c56c1b550
john.beckius <jbeckius@jacksondawson.com>
2018-08-02 14:08:46 -0400

    Changed appNode app filtering by currentVersion

--- src/components/appNode.vue
+++ src/components/appNode.vue
@@ -13,7 +13,7 @@
 					<button class="btn btn-primary settingsBtn" :data-id="appData.applicationId" v-on:click="select()">
 						Settings
 					</button>
-					<a v-if="iOS && currentVersion.path" :href="href" class="btn btn-secondary active" :disabled="currentVersion">Download</a>
+					<a v-if="iOS && currentVersion.downloadEnabled" :href="href" class="btn btn-secondary active" :disabled="currentVersion">Download</a>
 					<a v-if="!iOS" v-on:click="downloadApp" class="btn btn-secondary active downloadBtn">Download</a>
 					<p class="noApp" v-if="noApp">No download available for this device type</p>
 				</div>
@@ -71,7 +71,7 @@ export default {
 		},
 		currentVersion() {
 			return this.appData.versions.filter(version=>this.compatibleWithDevice(version))
-																	.find(version => version.downloadEnabled === 1) || new Object();
+																	.find(version => version.downloadEnabled === 1) || { downloadEnabled: 0, version: this.appData.versions[0].version};
 		},
 		imgPath() {
 			let baseURL = process.env.VUE_APP_BASE_CDN_URL;
