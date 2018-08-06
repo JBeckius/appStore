3a7ed92cfd8feda4f097a5e78dc225e168dba5e1
John Beckius <jbeckius@jd-10151.local>
2018-08-06 15:36:42 -0400

    AppModalMixin now returns fileSize in mb

--- src/components/modals/appModalMixin.js
+++ src/components/modals/appModalMixin.js
@@ -8,7 +8,7 @@ export default {
 		},
 		fileSize() {
 			let versions = this.appData.versions;
-			return (versions && versions.length) > 0 ? versions[0].fileSize : "No file size";
+			return (versions && versions.length) > 0 ? (versions[0].fileSize / 1000): "No file size";
 		},
 		lastUpdatedDate() {
 			return moment(this.appData.lastUpdated).format("MM/DD/YYYY");
